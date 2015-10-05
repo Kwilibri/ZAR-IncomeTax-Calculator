c20c21f717e26e41ec5e6763b5958879b4b93e48
Quintus Marais <quintus.marais@opsi.co.za>
2015-10-05 21:37:03 +0200

    Add rebates

--- src/ZARIncomeTax/Script.fsx
+++ src/ZARIncomeTax/Script.fsx
@@ -8,6 +8,10 @@ open Kwilibri.ZARIncomeTax
 let calc = new TaxCalculator()
 
 //This is not correct or tested yet.  Is not allocating the primary discount or taking age into account.
-System.Console.WriteLine( calc.ZARIncomeTax2016 100000)
+System.Console.WriteLine( calc.ZARIncomeTax2016 181900)
+System.Console.WriteLine( calc.ZARIncomeTax2016 284100)
+System.Console.WriteLine( calc.ZARIncomeTax2016 393200)
+System.Console.WriteLine( calc.ZARIncomeTax2016 550100)
+System.Console.WriteLine( calc.ZARIncomeTax2016 701300)
 
 
--- src/ZARIncomeTax/ZARIncomeTax.fs
+++ src/ZARIncomeTax/ZARIncomeTax.fs
@@ -5,7 +5,18 @@ open Kwilibri.ZARIncomeTax.TaxCalcUtilities
 open Kwilibri.ZARIncomeTax.ZARTaxRateTables
 
 type TaxCalculator() = 
-    member this.ZARIncomeTax2016 incomeAmount =
-        let result = ZARTaxRates2016 |> taxOf incomeAmount
+    member this.ZARIncomeTax2016 incomeAmount age =
+        let mutable result = ZARTaxRates2016 |> taxOf incomeAmount
+        let PrimaryRebate = 13257m
+        let SecondaryRebate = 7407m
+        let TertiaryRebate = 2466m
+
+        match age with
+        |i when i < 65 -> result <- result - PrimaryRebate
+        |j when j >= 65 && j<75 -> result <- result - PrimaryRebate - SecondaryRebate
+        |k when k >= 75 -> result <- result - PrimaryRebate - SecondaryRebate - TertiaryRebate
+
+        if (result <0m) then
+            result <- 0m
         result
         
